4b342a323da09afce3c7fc6c75bb4d0db59ba12b
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2015-06-30 18:25:25 +0300

    adding logging

--- src/main/java/ru/kupchinolabs/lat/StarterVertical.java
+++ src/main/java/ru/kupchinolabs/lat/StarterVertical.java
@@ -1,11 +1,16 @@
 package ru.kupchinolabs.lat;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.example.util.Runner;
-import io.vertx.ext.web.Router;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class StarterVertical extends AbstractVerticle {
+
+    private final static Logger log = Logger.getLogger(StarterVertical.class.getName());
 
     // Convenience method so you can run it in your IDE
     public static void main(String[] args) {
@@ -14,6 +19,19 @@ public class StarterVertical extends AbstractVerticle {
 
     @Override
     public void start() throws Exception {
-        vertx.deployVerticle(new HelloWorldVertical());
+        log.info("starting to deploy verticals");
+        vertx.deployVerticle(HelloWorldVertical.class.getName(), new AsyncResultHandler());
+    }
+
+    private class AsyncResultHandler implements Handler<AsyncResult<String>> {
+        @Override
+        public void handle(AsyncResult<String> event) {
+            if (event.succeeded()) {
+                log.info("deploy succeeded");
+            } else {
+                log.log(Level.SEVERE, "deploy failed: " + event.cause().getMessage());
+                vertx.close();
+            }
+        }
     }
 }
