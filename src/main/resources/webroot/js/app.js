8b1b38be50a1197d485e8cdf5097babc4c07a405
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2015-07-03 21:56:23 +0300

    adding filters

--- src/main/resources/webroot/js/app.js
+++ src/main/resources/webroot/js/app.js
@@ -1,10 +1,11 @@
 (function () {
     var app = angular.module('lat', []);
-    app.controller('ListController', ['$http', function ($http) {
+    app.controller('ListController', ['$http', '$scope', function ($http, $scope) {
+        this.contents = [];
         var list = this;
-        list.contents = [];
         $http.get('/api/list/').success(function(data){
             list.contents = data.contents;
+            $scope.orderProp = 'name';
         });
     }]);
 })();
