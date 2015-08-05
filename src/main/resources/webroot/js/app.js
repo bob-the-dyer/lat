0aebc5e7e8e3e5020dbf8856b7f5f50a38e1d618
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2015-08-05 17:03:27 +0300

    adding watch/unwatch requests

--- src/main/resources/webroot/js/app.js
+++ src/main/resources/webroot/js/app.js
@@ -3,15 +3,20 @@
     app.controller('ListController', ['$http', '$scope', function ($http, $scope) {
         this.contents = [];
         this.parent = "";
+        this.current = "";
         var list = this;
         $scope.openEntry = function (path, isDir) {
             console.log("trying to read " + path + ", encoded as " + encodeURIComponent(path));
             if (isDir) {
                 $http.get('/api/list/' + encodeURIComponent(path)).success(function (data) {
+                    if (list.current != null && list.current.length > 0){
+                        $http.get('/api/unwatch/' + encodeURIComponent(list.current));
+                    }
                     list.contents = data.contents;
                     list.parent = data.parent;
                     list.current = data.dir;
                     $scope.orderProp = 'name';
+                    $http.get('/api/watch/' + encodeURIComponent(list.current));
                 });
                 //TODO add a fail callback and display error on the UI
             } else {
