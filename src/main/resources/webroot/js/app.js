c546663b9d643b2c94c92d51beb9dd0f8e569ab2
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2015-08-05 16:11:35 +0300

    adding navigation to parent

--- src/main/resources/webroot/js/app.js
+++ src/main/resources/webroot/js/app.js
@@ -2,33 +2,42 @@
     var app = angular.module('lat', []);
     app.controller('ListController', ['$http', '$scope', function ($http, $scope) {
         this.contents = [];
+        this.parent = "";
         var list = this;
-        $http.get('/api/list/').success(function (data) {
-            list.contents = data.contents;
-            $scope.orderProp = 'name';
-        });
-        $scope.newread = function(entry) {
-            console.log("trying to read " + entry.path);
-            if (entry.isDirectory){
-                $http.get('/api/list/' + encodeURIComponent(entry.path)).success(function (data) {
+        $scope.openEntry = function (path, isDir) {
+            console.log("trying to read " + path + ", encoded as " + encodeURIComponent(path));
+            if (isDir) {
+                $http.get('/api/list/' + encodeURIComponent(path)).success(function (data) {
                     list.contents = data.contents;
+                    list.parent = data.parent;
                     $scope.orderProp = 'name';
                 });
+            } else {
+                console.log("opening files is not implemented yet");
             }
         };
-        $scope.classForEntry = function(entry){
-            if (entry.isDirectory){
+        $scope.classForEntry = function (entry) {
+            if (entry.isDirectory) {
                 return "list-group-item"
             } else {
                 return "list-group-item disabled"
             }
         };
-        $scope.glyphIconForEntry = function(entry){
-            if (entry.isDirectory){
+        $scope.classForParent = function (parent) {
+            console.log("classForParent called with " + parent);
+            if (parent != null && parent.length > 0) {
+                return "list-group-item"
+            } else {
+                return "ng-hide"
+            }
+        };
+        $scope.glyphIconForEntry = function (entry) {
+            if (entry.isDirectory) {
                 return "glyphicon glyphicon-folder-close"
             } else {
                 return "glyphicon glyphicon-file"
             }
-        }
+        };
+        $scope.openEntry("", true);
     }]);
 })();
