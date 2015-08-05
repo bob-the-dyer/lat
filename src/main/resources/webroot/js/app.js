c779c99d0a37f490052bcbaf8b76711601564783
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2015-08-05 13:39:49 +0300

    adding navigation down

--- src/main/resources/webroot/js/app.js
+++ src/main/resources/webroot/js/app.js
@@ -3,9 +3,25 @@
     app.controller('ListController', ['$http', '$scope', function ($http, $scope) {
         this.contents = [];
         var list = this;
-        $http.get('/api/list/').success(function(data){
+        $http.get('/api/list/').success(function (data) {
             list.contents = data.contents;
             $scope.orderProp = 'name';
         });
+        $scope.newread = function(entry) {
+            console.log("trying to read " + entry.path);
+            if (entry.isDirectory){
+                $http.get('/api/list/' + encodeURIComponent(entry.path)).success(function (data) {
+                    list.contents = data.contents;
+                    $scope.orderProp = 'name';
+                });
+            }
+        }
+        $scope.classForEntry = function(entry){
+            if (entry.isDirectory){
+                return "list-group-item"
+            } else {
+                return "list-group-item disabled"
+            }
+        }
     }]);
 })();
